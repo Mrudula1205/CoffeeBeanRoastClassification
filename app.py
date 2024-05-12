4146160a9b48b2e51a089d42f5334e7836cb02cc
unknown <mrudula12523@gmail.com>
2024-05-12 20:52:48 +0400

    new

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-'''import streamlit as st
+import streamlit as st
 import tensorflow as tf
 
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
-@st.cache
+@st.cache_data
 def load_model():
   model = tf.keras.models.load_model('vgg.hdf5')
   return model
@@ -47,79 +47,4 @@ else:
     predictions = import_and_predict(image, model=load_model())
     class_names = ['Dark', 'Green', 'Light', 'Medium']
     string = "This image most likely is: " + class_names[np.argmax(predictions)] + " with a confidence of " + str(round(np.max(predictions),2))
-    st.success(string)'''
-
-import streamlit as st
-import tensorflow as tf
-import cv2
-import numpy as np
-from PIL import Image, ImageOps
-
-# Function to load the model
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model = tf.keras.models.load_model('vgg.hdf5')
-    return model
-
-# Function to preprocess and make predictions on the image
-def import_and_predict(image_data, model):
-    size = (224, 224)
-    image = ImageOps.fit(image_data, size)
-    img = np.array(image, dtype='float32') / 255.0
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
-    return prediction
-
-# Main Streamlit app
-st.set_option('deprecation.showfileUploaderEncoding', False)
-st.title("Coffee Bean Quality Prediction App")
-st.markdown(
-    """
-    <style>
-    .st-emotion-cache-fg4pbf {
-        background-image: url("backgr.jpg");
-        background-size: cover;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# Sidebar to choose between uploading an image or using the camera
-option = st.sidebar.selectbox("Choose input source:", ("Upload Image", "Use Camera"))
-
-# If user chooses to upload an image
-if option == "Upload Image":
-    file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
-
-    if file is not None:
-        image = Image.open(file)
-        st.image(image, use_column_width=True)
-        predictions = import_and_predict(image, model=load_model())
-        class_names = ['Dark', 'Green', 'Light', 'Medium']
-        string = "This image most likely is: " + class_names[np.argmax(predictions)] + " with a confidence of " + str(
-            round(np.max(predictions), 2))
-        st.success(string)
-
-# If user chooses to use the camera
-elif option == "Use Camera":
-    cap = cv2.VideoCapture(0)
-    st.write("Please wait for the camera to load...")
-    if not cap.isOpened():
-        st.error("Unable to access camera.")
-    else:
-        st.write("Camera is ready.")
-        _, frame = cap.read()
-        img_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        img_pil = Image.fromarray(img_rgb)
-        st.image(img_pil, channels="RGB")
-
-        if st.button("Predict"):
-            predictions = import_and_predict(img_pil, model=load_model())
-            class_names = ['Dark', 'Green', 'Light', 'Medium']
-            string = "This image most likely is: " + class_names[np.argmax(predictions)] + " with a confidence of " + str(
-                round(np.max(predictions), 2))
-            st.success(string)
-
-            # Release the camera
-            cap.release()
+    st.success(string)
